9ca7354e9397627869861cb6f5b9f4fae307abe7
lihui1012 <1653581948@qq.com>
2017-10-16 19:32:34 +0800

    modify

---
+++ js/common.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	//自制的小屏菜单
+	$("#menu-click").click(function(){
+		$("#menu-totul").toggleClass("none");
+	});
+	$("#close").click(function(){
+		$("#menu-totul").addClass("none");
+	});
+});
